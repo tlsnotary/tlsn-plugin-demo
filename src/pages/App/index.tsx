5b9c8e86073077492cdfe4c1125a7ad5ad0b62db
Codetrauma <jtannershaw@gmail.com>
2024-09-30 17:07:12 -0700

    feat: added tlsn connect and extension installation check

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import "./index.scss";
 import { useDispatch } from 'react-redux';
 import './index.scss';
@@ -6,6 +6,40 @@ import Header from '../../components/Header';
 
 export default function App(): ReactElement {
   const dispatch = useDispatch();
+
+  const [extensionInstalled, setExtensionInstalled] = useState(false);
+
+  useEffect(() => {
+    const checkExtension = () => {
+      //@ts-ignore
+      if (window.tlsn && window.tlsn.installed) {
+        setExtensionInstalled(true);
+      } else {
+        return;
+      }
+    };
+
+    window.onload = () => {
+      checkExtension();
+    };
+
+    return () => {
+      window.onload = null;
+    };
+  }, []);
+
+  const InstallExtension = () => {
+    const extensionUrl = "https://chromewebstore.google.com/detail/tlsn-extension/gcfkkledipjbgdbimfpijgbkhajiaaph"; // Replace with your extension's URL
+      if (window.chrome && window.chrome.webstore) {
+        window.chrome.webstore.install(
+          extensionUrl,
+          () => alert("Extension installed successfully!"),
+          (err) => alert(`Error: ${err}`)
+        );
+      } else {
+        window.open(extensionUrl, "_blank");
+      }
+  }
 
   async function handleConnect() {
     //@ts-ignore
@@ -15,9 +49,11 @@ export default function App(): ReactElement {
   return (
     <div className="app flex flex-col gap-4">
       <Header />
-      <button onClick={handleConnect} className="button">
+      {extensionInstalled ?<button onClick={handleConnect} className="button">
         TLSN Connect
-      </button>
+      </button> : <button onClick={InstallExtension} className="button">
+        Install TLSN Extension
+      </button>}
     </div>
   );
 }
