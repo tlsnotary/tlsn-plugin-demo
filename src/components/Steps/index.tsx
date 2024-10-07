ffcd74fa58de78880ff68505c12bf241b2c295bb
Codetrauma <jtannershaw@gmail.com>
2024-10-07 16:03:32 -0700

    feat: step to run plugins

--- src/components/Steps/index.tsx
+++ src/components/Steps/index.tsx
@@ -4,10 +4,11 @@ import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import Box from '@mui/material/Box';
 import StepLabel from '@mui/material/StepLabel';
+import classNames from 'classnames';
 
 const steps = ['Connect Extension', 'Install Plugin', 'Run Plugin'];
 
-export default function Body(): ReactElement {
+export default function Steps(): ReactElement {
   const [extensionInstalled, setExtensionInstalled] = useState(false);
   const [pluginID, setPluginID] = useState('');
   const [step, setStep] = useState<number>(0);
@@ -56,10 +57,28 @@ export default function Body(): ReactElement {
     }
   }
 
+  async function handleRunPlugin() {
+    try {
+      const pluginData = await client.runPlugin(pluginID);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
   return (
     <div className="flex flex-col items-center gap-4">
       {extensionInstalled ? (
         <>
+          <div className="flex flex-row items-center gap-2 text-slate-600 font-bold pb-8">
+            Connected{' '}
+            <div
+              className={classNames('rounded-full h-[10px] w-[10px] border-[2px]', {
+                'bg-green-500': step >= 1,
+                'bg-red-500': step === 0,
+              })}
+            ></div>
+          </div>
           <div className="flex gap-3">
             {step === 0 && (
               <button onClick={handleConnect} className="button">
@@ -71,7 +90,7 @@ export default function Body(): ReactElement {
                 Install Plugin
               </button>
             )}
-            {step === 2 && <button className="button">Run Plugin</button>}
+            {step === 2 && <button onClick={handleRunPlugin} className="button">Run Plugin</button>}
           </div>
           <Box className="w-full max-w-md mt-6">
             <Stepper activeStep={step} alternativeLabel>
@@ -95,4 +114,3 @@ export default function Body(): ReactElement {
     </div>
   );
 }
-
