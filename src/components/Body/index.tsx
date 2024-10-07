344110855ed5a34a204b7f7c22ab446d5b5da37f
Codetrauma <jtannershaw@gmail.com>
2024-10-07 15:31:22 -0700

    feat: stepper for plugin installation

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -1,69 +1,23 @@
 import React, { ReactElement, useEffect, useState } from 'react';
 import './index.scss';
+import Stepper from '../Steps';
+import Icon from '../Icon'
+import Logo from '../../../static/logo.svg';
+
+
+const steps = ['Connect Extension', 'Install Plugin', 'Run Plugin'];
+
 export default function Body(): ReactElement {
-  const [extensionInstalled, setExtensionInstalled] = useState(false);
-  const [pluginID, setPluginID] = useState('');
 
 
-  let client: any;
-
-  useEffect(() => {
-    const checkExtension = () => {
-      //@ts-ignore
-      if (typeof window.tlsn !== 'undefined') {
-        setExtensionInstalled(true);
-      } else {
-        return;
-      }
-    };
-
-    window.onload = () => {
-      checkExtension();
-    };
-
-    return () => {
-      window.onload = null;
-    };
-  }, []);
-
-  async function handleConnect() {
-    try {
-      //@ts-ignore
-      client = await window.tlsn.connect();
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  async function handlePluginInstall() {
-    try {
-      const plugin = await client.installPlugin('https://github.com/tlsnotary/tlsn-extension/raw/main/src/assets/plugins/twitter_profile.wasm', { id: 'twitter-plugin' });
-      setPluginID(plugin);
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
   return (
-    <div className="flex flex-row justify-center gap-3">
-      {extensionInstalled ? (
-        <div>
-          <button onClick={handleConnect} className="button">
-            TLSN Connect
-          </button>
-          <button onClick={handlePluginInstall} className="button">
-            Install Plugin
-          </button>
-        </div>
-      ) : (
-        <a
-          href="https://chromewebstore.google.com/detail/tlsn-extension/gcfkkledipjbgdbimfpijgbkhajiaaph"
-          target="_blank"
-          className="button"
-        >
-          Install TLSN Extension
-        </a>
-      )}
+    <div className="w-full">
+    <div className="flex flex-row w-full justify-center items-center gap-4 pb-12">
+      <img className="w-12 h-12" src={Logo} alt="Logo" />
+      <span className="font-bold text-slate-600 text-3xl">Plugin Demo</span>
     </div>
-  );
+    <Stepper />
+  </div>
+  )
 }
---
+++ src/components/Steps/index.tsx
@@ -0,0 +1,98 @@
+import React, { ReactElement, useEffect, useState } from 'react';
+import './index.scss';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import Box from '@mui/material/Box';
+import StepLabel from '@mui/material/StepLabel';
+
+const steps = ['Connect Extension', 'Install Plugin', 'Run Plugin'];
+
+export default function Body(): ReactElement {
+  const [extensionInstalled, setExtensionInstalled] = useState(false);
+  const [pluginID, setPluginID] = useState('');
+  const [step, setStep] = useState<number>(0);
+  const [client, setClient] = useState<any>(null);
+
+  useEffect(() => {
+    const checkExtension = () => {
+      //@ts-ignore
+      if (typeof window.tlsn !== 'undefined') {
+        setExtensionInstalled(true);
+      } else {
+        return;
+      }
+    };
+
+    window.onload = () => {
+      checkExtension();
+    };
+
+    return () => {
+      window.onload = null;
+    };
+  }, []);
+
+  async function handleConnect() {
+    try {
+      //@ts-ignore
+      setClient(await window.tlsn.connect());
+      setStep(1);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handlePluginInstall() {
+    try {
+      console.log(client);
+      const plugin = await client.installPlugin(
+        'https://github.com/tlsnotary/tlsn-extension/raw/main/src/assets/plugins/twitter_profile.wasm',
+        { id: 'twitter-plugin' },
+      );
+      setPluginID(plugin);
+      setStep(2);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="flex flex-col items-center gap-4">
+      {extensionInstalled ? (
+        <>
+          <div className="flex gap-3">
+            {step === 0 && (
+              <button onClick={handleConnect} className="button">
+                Connect
+              </button>
+            )}
+            {step === 1 && (
+              <button onClick={handlePluginInstall} className="button">
+                Install Plugin
+              </button>
+            )}
+            {step === 2 && <button className="button">Run Plugin</button>}
+          </div>
+          <Box className="w-full max-w-md mt-6">
+            <Stepper activeStep={step} alternativeLabel>
+              {steps.map((label) => (
+                <Step key={label}>
+                  <StepLabel>{label}</StepLabel>
+                </Step>
+              ))}
+            </Stepper>
+          </Box>
+        </>
+      ) : (
+        <a
+          href="https://chromewebstore.google.com/detail/tlsn-extension/gcfkkledipjbgdbimfpijgbkhajiaaph"
+          target="_blank"
+          className="px-4 py-2 bg-blue-500 text-white rounded-md"
+        >
+          Install TLSN Extension
+        </a>
+      )}
+    </div>
+  );
+}
+
