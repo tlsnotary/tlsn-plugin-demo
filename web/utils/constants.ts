48d0e225fb630841ff4ecb24bf081a11d65e9085
Codetrauma <jtannershaw@gmail.com>
2024-11-14 12:46:22 +0700

    fix: small text change

--- web/utils/constants.ts
+++ web/utils/constants.ts
@@ -1,1 +1,2 @@
-export const localPem = "-----BEGIN PUBLIC KEY-----\nMDYwEAYHKoZIzj0CAQYFK4EEAAoDIgADe0jxnBObaIj7Xjg6TXLCM1GG/VhY5650\nOrS/jgcbBuc=\n-----END PUBLIC KEY-----\n";
+export const localPem =
+  '-----BEGIN PUBLIC KEY-----\nMDYwEAYHKoZIzj0CAQYFK4EEAAoDIgADe0jxnBObaIj7Xjg6TXLCM1GG/VhY5650\nOrS/jgcbBuc=\n-----END PUBLIC KEY-----\n';
