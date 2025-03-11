6793a8937673439120261c1fd6c6ec01646f890b
Nick Tampakas <nick@gmx.co.uk>
2025-03-11 09:15:43 +0200

    Build image using firebase cfg

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # Download poaps
-aws s3 cp s3://tlsn-plugin-demo/poaps.txt server/util/
+aws s3 cp s3://tlsn-plugin-demo/firebase-admin.json server/util/
 
 aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 490752553772.dkr.ecr.eu-central-1.amazonaws.com
 
